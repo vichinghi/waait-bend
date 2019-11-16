87b0d3bdd7cd21e7d3001a3a241eae280bc9003b
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-11-16 17:15:03 +0300

    add many to many relationship

--- apps/incidents/models.py
+++ apps/incidents/models.py
@@ -19,13 +19,13 @@ class Website(SurrogatePK, Model):
 class IncidentReport(SurrogatePK, Model):
     __tablename__ = 'incident_reports'
 
-    criminal = db.Column(db.String)
     what_happened = db.Column(db.String)
     how_it_happend = db.Column(db.String)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
     where_it_happend = db.Column(db.String)
     website = db.Column(db.String)
     url = db.Column(db.String)
+    criminals = db.relationship('Criminal', lazy=True, secondary="criminal_incident_association",)
 
 
 association_table = db.Table('criminal_incident_association',
--- apps/incidents/schema.py
+++ apps/incidents/schema.py
@@ -5,7 +5,13 @@ from ..utils.base_schema import BaseSchema
 from .models import IncidentReport, Website, Criminal
 
 
+class CriminalSchema(BaseSchema):
+    class Meta:
+        model = Criminal
+
+
 class IncidentReportSchema(BaseSchema):
+    criminals = fields.Nested(CriminalSchema, many=True)
 
     class Meta:
         model = IncidentReport
